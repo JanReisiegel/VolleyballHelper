5f26d16445f8d0da3960bcaa4ff2a0e2505d9a54
Jan_Reisiegel <jan.reisiegel@tul.cz>
2024-09-14 16:10:56 +0200

    Start editing create fragment

--- app/src/main/java/com/reisiegel/volleyballhelper/ui/home/HomeFragment.kt
+++ app/src/main/java/com/reisiegel/volleyballhelper/ui/home/HomeFragment.kt
@@ -4,10 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.reisiegel.volleyballhelper.databinding.FragmentHomeBinding
+import java.io.File
 
 class HomeFragment : Fragment() {
 
@@ -28,10 +30,25 @@ class HomeFragment : Fragment() {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+
+        val statisticsDirectory = File(context?.filesDir, "Statistics")
+        if (!statisticsDirectory.isDirectory)
+            statisticsDirectory.mkdir()
+        var statisticFiles = statisticsDirectory.listFiles()
+
+        var filesAdapter = mutableMapOf<String, String>()
+        var listFileNames = ArrayList<String>()
+
+        statisticFiles.forEach { file: File ->
+            filesAdapter[file.name] = file.path
+            listFileNames.add(file.name)
         }
+
+        var filesView: Spinner = binding.spinner
+        var adapter = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, listFileNames)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        filesView.adapter = adapter
+
         return root
     }
 
