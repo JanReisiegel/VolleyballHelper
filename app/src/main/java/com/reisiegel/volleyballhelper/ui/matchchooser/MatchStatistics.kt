387406ded1da0ff0c7b9b3ed300fc4900372b791
Jan_Reisiegel <jan.reisiegel@tul.cz>
2025-02-03 00:02:15 +0100

    Android studio update (gradle update) + prepare for functionality

--- app/src/main/java/com/reisiegel/volleyballhelper/ui/matchchooser/MatchStatistics.kt
+++ app/src/main/java/com/reisiegel/volleyballhelper/ui/matchchooser/MatchStatistics.kt
@@ -6,9 +6,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.reisiegel.volleyballhelper.R
 import com.reisiegel.volleyballhelper.databinding.FragmentMatchStatisticsBinding
 import com.reisiegel.volleyballhelper.ui.matchchooser.MatchAdapter
 import com.reisiegel.volleyballhelper.ui.matchchooser.MatchItem
@@ -21,6 +25,7 @@ class MatchStatistics : Fragment() {
     private lateinit var viewModel: MatchStatisticsViewModel
     private var _binding: FragmentMatchStatisticsBinding? = null
     private val binding get() = _binding!!
+
 
     private lateinit var recycleMatchesView: RecyclerView
 
@@ -63,6 +68,37 @@ class MatchStatistics : Fragment() {
 
         matchList?.forEach {
             match -> viewModel.addMatchItem(MatchItem(match.opponentName, match.startTime))
+        }
+
+        val zoneIds = listOf(
+            R.id.zone1, R.id.zone6, R.id.zone5,
+            R.id.zone2, R.id.zone3, R.id.zone4,
+        )
+
+        zoneIds.forEachIndexed { index, zoneId ->
+            val zoneView = root.findViewById<View>(zoneId)
+            val attackBlockLayout = zoneView.findViewById<LinearLayout>(R.id.attack_block_layout)
+            val receptionLayout = zoneView.findViewById<LinearLayout>(R.id.reception_layout)
+            val serviceLayout = zoneView.findViewById<LinearLayout>(R.id.service_layout)
+            val playerName = zoneView.findViewById<TextView>(R.id.player_name)
+            val playerNumber = zoneView.findViewById<TextView>(R.id.player_number)
+            val setPlayer = zoneView.findViewById<Button>(R.id.set_to_zone)
+            val serviceAce = zoneView.findViewById<Button>(R.id.service_ace)
+            val serviceError = zoneView.findViewById<Button>(R.id.service_error)
+            val serviceReceived = zoneView.findViewById<Button>(R.id.service_received)
+            val attackError = zoneView.findViewById<Button>(R.id.attack_error)
+            val attackHit = zoneView.findViewById<Button>(R.id.attack_hit)
+            val attackReceived = zoneView.findViewById<Button>(R.id.attack_received)
+            val attackBlocked = zoneView.findViewById<Button>(R.id.attack_block)
+            val blockPoint = zoneView.findViewById<Button>(R.id.block_point)
+            val blockError = zoneView.findViewById<Button>(R.id.block_error)
+            val blockNoPoint = zoneView.findViewById<Button>(R.id.block_no_point)
+            val receptionIdeal = zoneView.findViewById<Button>(R.id.reception_ideal)
+            val receptionContinue = zoneView.findViewById<Button>(R.id.reception_continue)
+            val receptionError = zoneView.findViewById<Button>(R.id.reception_error)
+            val receptionNoContinue = zoneView.findViewById<Button>(R.id.reception_no_continue)
+
+
         }
 
 
--- app/src/main/java/com/reisiegel/volleyballhelper/ui/matchchooser/MatchStatisticsViewModel.kt
+++ app/src/main/java/com/reisiegel/volleyballhelper/ui/matchchooser/MatchStatisticsViewModel.kt
@@ -3,12 +3,16 @@ package com.reisiegel.volleyballhelper.ui.matchchooser
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.reisiegel.volleyballhelper.models.Player
 import com.reisiegel.volleyballhelper.ui.matchchooser.MatchItem
 
 class MatchStatisticsViewModel : ViewModel() {
     private val _matchList = MutableLiveData<MutableList<MatchItem>>()
+    private val _playersSquad = MutableLiveData<MutableList<Player>>()
 
     val matchList: LiveData<MutableList<MatchItem>> = _matchList
+    val playersSquad: LiveData<MutableList<Player>> = _playersSquad
+
 
     fun addMatchItem(match: MatchItem){
         val updatedMatchList = _matchList.value ?: mutableListOf()
