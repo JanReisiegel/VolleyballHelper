531b18b08db9a06835de56588b4a6d9c3e98e47a
JanReisiegel <jan.reisiegel@gmail.com>
2025-03-21 22:31:26 +0100

    SignIn and SignOut is done thanks to firebase

--- app/src/main/java/com/reisiegel/volleyballhelper/ui/export/ExportStatistics.kt
+++ app/src/main/java/com/reisiegel/volleyballhelper/ui/export/ExportStatistics.kt
@@ -14,9 +14,11 @@ import androidx.credentials.Credential
 import androidx.credentials.CredentialManager
 import androidx.credentials.CustomCredential
 import androidx.credentials.GetCredentialRequest
+import androidx.credentials.GetCredentialResponse
 import androidx.credentials.exceptions.ClearCredentialException
 import androidx.credentials.exceptions.GetCredentialCancellationException
 import androidx.credentials.exceptions.GetCredentialException
+import androidx.credentials.exceptions.NoCredentialException
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
@@ -25,17 +27,17 @@ import com.google.android.libraries.identity.googleid.GetSignInWithGoogleOption
 import com.google.android.libraries.identity.googleid.GoogleIdTokenCredential
 import com.google.android.libraries.identity.googleid.GoogleIdTokenCredential.Companion.TYPE_GOOGLE_ID_TOKEN_CREDENTIAL
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.auth.auth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.reisiegel.volleyballhelper.R
 import com.reisiegel.volleyballhelper.databinding.FragmentExportStatisticsBinding
 import com.reisiegel.volleyballhelper.services.GoogleDriveService
 import kotlinx.coroutines.launch
 
 class ExportStatistics : Fragment() {
-
-    companion object {
-        private const val TAG = "GoogleActivity"
-    }
 
     private lateinit var viewModel: ExportStatisticsViewModel
     private var _binding: FragmentExportStatisticsBinding? = null
@@ -44,6 +46,9 @@ class ExportStatistics : Fragment() {
 
     private lateinit var auth: FirebaseAuth
     private lateinit var credentialManager: CredentialManager
+    private val TAG = "GoogleActivity"
+    //private val TYPE_GOOGLE_ID_TOKEN_CREDENTIAL = "com.google.android.libraries.identity.googleid.TYPE_GOOGLE_ID_TOKEN_CREDENTIAL"
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -52,56 +57,59 @@ class ExportStatistics : Fragment() {
         viewModel = ViewModelProvider(this).get(ExportStatisticsViewModel::class.java)
         _binding = FragmentExportStatisticsBinding.inflate(inflater, container, false)
         val root: View = binding.root
-
-        auth = FirebaseAuth.getInstance()
-        credentialManager = CredentialManager.create(root.context)
-
-        googleDriveService = GoogleDriveService(root.context)
-
-        binding.exportButton.setOnClickListener {
-            launchCredentialManager()
-            //auth.signInWithCustomToken(root.context.getString(R.string.default_web_client_id))
-        }
-
-        val currentUser = auth.currentUser
-
         return root
     }
 
-    private fun launchCredentialManager() {
-        val googleIdOption = GetSignInWithGoogleOption.Builder(binding.root.context.getString(R.string.default_web_client_id))
-            //.setServerClientId(binding.root.context.getString(R.string.default_web_client_id))
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        auth = Firebase.auth
+
+
+        binding.exportButton.setOnClickListener {
+            var user = auth.currentUser
+            if (user?.displayName != null) {
+                signOut()
+            } else {
+                signInRequest()
+                Log.d(TAG, "User is signed in: ${auth.currentUser?.email}")
+            }
+
+
+        }
+
+    }
+
+    private fun signInRequest(){
+        // Instantiate a Google sign-in request
+        val googleIdOption = GetGoogleIdOption.Builder()
+            .setServerClientId(getString(R.string.default_web_client_id))
+            .setFilterByAuthorizedAccounts(false) // Set to false to allow new accounts
             .build()
 
+        // Create the Credential Manager request
         val request = GetCredentialRequest.Builder()
             .addCredentialOption(googleIdOption)
             .build()
 
-        lifecycleScope.launch {
-            try {
-                // Launch Credential Manager UI
-                val result = credentialManager.getCredential(
-                    context = binding.root.context,
-                    request = request
-                )
+        // Begin the sign-in process
+        beginSignIn(request)
+    }
 
-                Log.d(TAG, "Credential Manager Result: $result") // Loguj celý výsledek
-                if (result.credential != null) {
-                    handleSignIn(result.credential)
-                    Log.d(TAG, "Credential: ${result.credential}")
-                } else {
-                    Log.e(TAG, "Credential is null")
-                }
-            } catch (e: GetCredentialCancellationException) {
-                Log.w(TAG, "Přihlášení zrušeno uživatelem.")
-                // Zobraz uživateli zprávu, že přihlášení bylo zrušeno
-                Toast.makeText(binding.root.context, "Přihlášení zrušeno.", Toast.LENGTH_SHORT).show()
+    private fun beginSignIn(request: GetCredentialRequest){
+        val credentialManager = CredentialManager.create(requireContext())
+        lifecycleScope.launch {
+            try{
+                val result = credentialManager.getCredential(requireActivity(), request)
+                val credential = result.credential
+                handleSignIn(credential)
             }catch (e: GetCredentialException) {
-                Log.e(TAG, "Couldn't retrieve user's credentials: ${e.localizedMessage}")
-                Log.e(TAG, "GetCredentialException: $e") // Loguj celou výjimku
-            } catch (e: Exception) {
-                Log.e(TAG, "General Exception: ${e.localizedMessage}")
-                Log.e(TAG, "Exception: $e")
+                if (e is NoCredentialException) {
+                    Log.e(TAG, "Couldn't retrieve user's credentials: ${e.message}")
+                    Log.e(TAG, "GetCredentialException: ${e.javaClass.name}: ${e.message}")
+                } else {
+                    Log.e(TAG, "GetCredentialException: ${e.javaClass.name}: ${e.message}")
+                }
             }
         }
     }
@@ -129,17 +137,7 @@ class ExportStatistics : Fragment() {
                     // Sign in success, update UI with the signed-in user's information
                     Log.d(TAG, "signInWithCredential:success")
                     val user = auth.currentUser
-                    if (user != null) {
-                        val account = user.providerData.find { it.providerId == "google.com" }?.let {
-                            Account(it.email!!, "com.google")
-                        }
-                        if (account != null) {
-                            googleDriveService.createGoogleSheet(account)
-                        } else {
-                            Log.e(TAG, "Couldn't retrieve user's account")
-                        }
-                    }
-                    binding.root.requestLayout()
+                    binding.exportButton.text = user?.email
                 } else {
                     // If sign in fails, display a message to the user
                     Log.w(TAG, "signInWithCredential:failure", task.exception)
@@ -153,15 +151,21 @@ class ExportStatistics : Fragment() {
         // Firebase sign out
         auth.signOut()
 
-        // When a user signs out, clear the current user credential state from all credential providers.
-        lifecycleScope.launch {
-            try {
-                val clearRequest = androidx.credentials.ClearCredentialStateRequest()
-                credentialManager.clearCredentialState(clearRequest)
-                binding.root.requestLayout()//updateUI(null)
-            } catch (e: ClearCredentialException) {
-                Log.e(TAG, "Couldn't clear user credentials: ${e.localizedMessage}")
-            }
+        binding.exportButton.text = "Export"
+        // Update UI
+        updateUI(null)
+
+        Log.d(TAG, "User signed out")
+    }
+
+    private fun updateUI(user: FirebaseUser?) {
+        // Update your UI based on the user's sign-in status
+        if (user != null) {
+            // User is signed in
+            Log.d(TAG, "User is signed in: ${user.email}")
+        } else {
+            // User is signed out
+            Log.d(TAG, "User is signed out")
         }
     }
 }
